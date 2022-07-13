e04d06a1d861bc55d691aba4189257c5afd561bf
Mahender <codingmahender@gmail.com>
2022-07-13 08:11:18 +0530

    if commit

---
+++ oneif.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int maun()
+{
+    int age;
+    printf("enter age : ");
+    scanf("%i",&age);
+    if (age>18){
+    printf("adult\n");
+    printf("they can voate \n");
+    }
+    else 
+    {
+    printf("not adult \n");
+
+    }
+    printf("Thank You for visit");
+
+}
